eb907778a53e7c4db7b344f1319aeca596aa2f7b
themodulator <mearly1973@gmail.com>
2015-08-11 00:27:27 -0400

    Created Import preview for directory member

--- Areas/Manage/Controllers/MemberImportController.cs
+++ Areas/Manage/Controllers/MemberImportController.cs
@@ -4,24 +4,47 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Lcps.Division.Directory.Providers;
+using Lcps.Division.Directory.Repository;
+using Lcps.Division.Directory.API.Areas.Manage.Models;
+using PagedList;
 
 namespace Lcps.Division.Directory.API.Areas.Manage.Controllers
 {
     public class MemberImportController : Controller
     {
+        public const string varName = "DiretcoryMember";
+
+
         // GET: Manage/MemberImport
         public ActionResult Index()
         {
 
-            ImportTextFile m = new ImportTextFile("Import Division Members", "\t", new MvcRouteDefinition() { Action = "Preview", Area = "Manage", Controller = "MemberImport" });
+            ImportTextFile<DirectoryMember> m = new ImportTextFile<DirectoryMember>("Import Division Members", "\t", new MvcRouteDefinition() { Action = "Preview", Area = "Manage", Controller = "MemberImport" });
             return View(m);
         }
 
         [HttpPost]
-        public ActionResult Preview(ImportTextFile txt)
+        public ActionResult Preview(ImportTextFile<DirectoryMember> txt)
         {
             txt.Load();
-            return View(txt);
+            Session[varName] = txt;
+            return RedirectToAction("MemberPreview");
+
+            
+        }
+
+        public ActionResult MemberPreview(int? page)
+        {
+            page = (page == null) ? 1 : page;
+
+            ImportTextFile<DirectoryMember> items = (ImportTextFile<DirectoryMember>)Session[varName];
+
+            ViewBag.Total = items.Items.Count();
+            ViewBag.Page = page.Value;
+
+            PagedList<DirectoryMember> members = new PagedList<DirectoryMember>(items.Items, page.Value, 12);
+
+            return View(members);
         }
     }
 }
---
+++ Areas/Manage/Models/DirectoryMemberListModel.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PagedList;
+
+using Lcps.Division.Directory.Providers;
+
+
+using Lcps.Division.Directory.Repository;
+
+namespace Lcps.Division.Directory.API.Areas.Manage.Models
+{
+    public class DirectoryMemberListModel
+    {
+        public bool AllowEdit { get; set; }
+
+        public PagedList<DirectoryMember> Members { get; set; }
+
+        public MvcRouteDefinition SourceRoute { get; set; }
+    }
+}
